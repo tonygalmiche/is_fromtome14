778dfe9b21b48ee0aecd02f12902c88206cc4e93
Tony Galmiche <tony.galmiche@infosaone.com>
2021-10-01 09:59:57 +0200

    modif

--- models/sale_order.py
+++ models/sale_order.py
@@ -142,72 +142,73 @@ class SaleOrder(models.Model):
                     raise Warning(u"La date de réception n'est pas renseignée sur toutes les lignes")
             now = datetime.date.today()
             for line in obj.order_line:
-                suppliers=self.env['product.supplierinfo'].search([('product_tmpl_id', '=', line.product_id.product_tmpl_id.id)])
-                partner_id=False
-                supplierinfo=False
-                for s in suppliers:
-                    if now>=s.date_start and now<= s.date_end:
-                        supplierinfo=s
-                        break
-                if supplierinfo:
-                    partner_id = supplierinfo.name.id
-                    date_reception = str(line.is_date_reception)
-                    date_planned  = date_reception+' 08:00:00'
-                    filtre=[
-                        ('partner_id'  ,'='   , partner_id),
-                        ('state'       ,'='   , 'draft'),
-                        ('date_planned','>=', date_reception+' 00:00:00'),
-                        ('date_planned','<=', date_reception+' 23:59:59'),
-                    ]
-                    if line.is_livraison_directe:
-                        filtre.append(('delivery_adress','=', obj.partner_shipping_id.id))
-                    orders=self.env['purchase.order'].search(filtre,limit=1)
-                    if orders:
-                        order=orders[0]
-                    else:
-                        vals={
-                            'partner_id'  : partner_id,
-                        }
-                        order=self.env['purchase.order'].create(vals)
-                        if order:
-                            order.onchange_partner_id()
-                            if line.is_livraison_directe:
-                                order.delivery_adress = obj.partner_shipping_id.id
-
-                    #** Création des lignes ************************************
-                    filtre=[
-                        ('order_id'  ,'=', order.id),
-                        ('product_id','=', line.product_id.id),
-                    ]
-                    order_lines=self.env['purchase.order.line'].search(filtre,limit=1)
-                    if not order_lines:
-                        if order:
-                            vals={
-                                'order_id'    : order.id,
-                                'product_id'  : line.product_id.id,
-                                'name'        : line.name,
-                                'product_qty' : line.product_uom_qty,
-                                'product_uom' : line.product_uom.id,
-                                'date_planned': date_planned,
-                                'price_unit'  : line.price_unit,
-                                #'is_sale_order_line_id': line.id,
-                            }
-                            order_line=self.env['purchase.order.line'].create(vals)
-                            order_line.onchange_product_id()
-                    else:
-                        order_line = order_lines[0]
-
-                    if order_line:
-                        line.is_purchase_line_id=order_line.id
+                if not line.is_purchase_line_id:
+                    suppliers=self.env['product.supplierinfo'].search([('product_tmpl_id', '=', line.product_id.product_tmpl_id.id)])
+                    partner_id=False
+                    supplierinfo=False
+                    for s in suppliers:
+                        if now>=s.date_start and now<= s.date_end:
+                            supplierinfo=s
+                            break
+                    if supplierinfo:
+                        partner_id = supplierinfo.name.id
+                        date_reception = str(line.is_date_reception)
+                        date_planned  = date_reception+' 08:00:00'
                         filtre=[
-                            ('is_purchase_line_id'  ,'=', order_line.id),
+                            ('partner_id'  ,'='   , partner_id),
+                            ('state'       ,'='   , 'draft'),
+                            ('date_planned','>=', date_reception+' 00:00:00'),
+                            ('date_planned','<=', date_reception+' 23:59:59'),
                         ]
-                        order_lines=self.env['sale.order.line'].search(filtre)
-                        qty=0
-                        for l in order_lines:
-                            qty+=l.product_uom_qty
-                        order_line.product_qty = qty
-                    # ***********************************************************
+                        if line.is_livraison_directe:
+                            filtre.append(('delivery_adress','=', obj.partner_shipping_id.id))
+                        orders=self.env['purchase.order'].search(filtre,limit=1)
+                        if orders:
+                            order=orders[0]
+                        else:
+                            vals={
+                                'partner_id'  : partner_id,
+                            }
+                            order=self.env['purchase.order'].create(vals)
+                            if order:
+                                order.onchange_partner_id()
+                                if line.is_livraison_directe:
+                                    order.delivery_adress = obj.partner_shipping_id.id
+
+                        #** Création des lignes ************************************
+                        filtre=[
+                            ('order_id'  ,'=', order.id),
+                            ('product_id','=', line.product_id.id),
+                        ]
+                        order_lines=self.env['purchase.order.line'].search(filtre,limit=1)
+                        if not order_lines:
+                            if order:
+                                vals={
+                                    'order_id'    : order.id,
+                                    'product_id'  : line.product_id.id,
+                                    'name'        : line.name,
+                                    'product_qty' : line.product_uom_qty,
+                                    'product_uom' : line.product_uom.id,
+                                    'date_planned': date_planned,
+                                    'price_unit'  : line.price_unit,
+                                    #'is_sale_order_line_id': line.id,
+                                }
+                                order_line=self.env['purchase.order.line'].create(vals)
+                                order_line.onchange_product_id()
+                        else:
+                            order_line = order_lines[0]
+
+                        if order_line:
+                            line.is_purchase_line_id=order_line.id
+                            filtre=[
+                                ('is_purchase_line_id'  ,'=', order_line.id),
+                            ]
+                            order_lines=self.env['sale.order.line'].search(filtre)
+                            qty=0
+                            for l in order_lines:
+                                qty+=l.product_uom_qty
+                            order_line.product_qty = qty
+                        # ***********************************************************
 
 
 
