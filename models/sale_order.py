191e6179d6138a2966cb3765d68e95d60f10e28b
Tony Galmiche <tony.galmiche@infosaone.com>
2022-03-02 10:15:46 +0100

    modifs

--- models/sale_order.py
+++ models/sale_order.py
@@ -93,6 +93,62 @@ class SaleOrderLine(models.Model):
             return res
 
 
+
+    # def _get_display_price(self, product):
+
+    #     print("### TEST 1 ####")
+
+    #     # TO DO: move me in master/saas-16 on sale.order
+    #     # awa: don't know if it's still the case since we need the "product_no_variant_attribute_value_ids" field now
+    #     # to be able to compute the full price
+
+    #     # it is possible that a no_variant attribute is still in a variant if
+    #     # the type of the attribute has been changed after creation.
+    #     no_variant_attributes_price_extra = [
+    #         ptav.price_extra for ptav in self.product_no_variant_attribute_value_ids.filtered(
+    #             lambda ptav:
+    #                 ptav.price_extra and
+    #                 ptav not in product.product_template_attribute_value_ids
+    #         )
+    #     ]
+    #     if no_variant_attributes_price_extra:
+    #         product = product.with_context(
+    #             no_variant_attributes_price_extra=tuple(no_variant_attributes_price_extra)
+    #         )
+
+
+    #     cr,uid,context,su = self.env.args
+
+
+    #     print("### TEST 2 ####",context)
+
+
+    #     if self.order_id.pricelist_id.discount_policy == 'with_discount':
+
+    #         print("### TEST 3 ####",product.with_context(pricelist=self.order_id.pricelist_id.id).price)
+
+
+    #         return product.with_context(pricelist=self.order_id.pricelist_id.id).price
+    #     #product_context = dict(self.env.context, partner_id=self.order_id.partner_id.id, date=self.order_id.date_order, uom=self.product_uom.id)
+    #     product_context = dict(self.env.context, partner_id=self.order_id.partner_id.id, date=self.order_id.is_date_livraison, uom=self.product_uom.id)
+
+    #     final_price, rule_id = self.order_id.pricelist_id.with_context(product_context).get_product_price_rule(product or self.product_id, self.product_uom_qty or 1.0, self.order_id.partner_id)
+    #     base_price, currency = self.with_context(product_context)._get_real_price_currency(product, rule_id, self.product_uom_qty, self.product_uom, self.order_id.pricelist_id.id)
+    #     if currency != self.order_id.pricelist_id.currency_id:
+    #         base_price = currency._convert(
+    #             base_price, self.order_id.pricelist_id.currency_id,
+    #             #self.order_id.company_id or self.env.company, self.order_id.date_order or fields.Date.today())
+    #             self.order_id.company_id or self.env.company, self.order_id.is_date_livraison or fields.Date.today())
+    #     # negative discounts (= surcharge) are included in the display price
+
+    #     print(base_price, final_price)
+
+    #     return max(base_price, final_price)
+
+
+
+
+
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
