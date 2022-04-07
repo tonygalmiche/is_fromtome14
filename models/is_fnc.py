99fa54f52270f1b10f66f60ba50deb426467124e
Tony Galmiche <tony.galmiche@infosaone.com>
2022-04-07 15:10:22 +0200

    modifs

--- models/is_fnc.py
+++ models/is_fnc.py
@@ -3,6 +3,7 @@ from odoo import api, fields, models, _
 
 class IsFNC(models.Model):
     _name = 'is.fnc'
+    _inherit = ['mail.thread']
     _description = "Fiche de non-conformité Client / Fournisseur"
     _order = 'name desc'
 
