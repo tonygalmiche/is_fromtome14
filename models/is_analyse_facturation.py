45ba471305c6277697e85d5443e0819f028d2eb6
Tony Galmiche <tony.galmiche@infosaone.com>
2022-12-11 13:37:59 +0100

    modifs

--- models/is_analyse_facturation.py
+++ models/is_analyse_facturation.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
 from datetime import datetime, timedelta, date
+import logging
+_logger = logging.getLogger(__name__)
 
 
 _MOVE_TYPE={
@@ -19,83 +21,150 @@ class IsAnalyseFacturationUpdate(models.TransientModel):
     date_debut = fields.Date('Date début', required=True, default=lambda self: fields.Datetime.now()-timedelta(30))
     date_fin   = fields.Date('Date fin'  , required=True, default=lambda self: fields.Datetime.now())
 
+
     def action_ok(self):
+        self.update(self.date_debut,self.date_fin)
+
+
+    def update(self, date_debut, date_fin):
+        cr, user, context, su = self.env.args
         filtre=[
-            ('invoice_date','>=',self.date_debut),
-            ('invoice_date','<=',self.date_fin),
+            ('invoice_date','>=',date_debut),
+            ('invoice_date','<=',date_fin),
+            ('bloquer','=', True),
         ]
-        self.env['is.analyse.facturation'].search(filtre).unlink()
+        lines=self.env['is.analyse.facturation'].search(filtre)
+        invoice_line_ids=scrap_ids=[]
+        for line in lines:
+            if line.invoice_line_id:
+                invoice_line_ids.append(line.invoice_line_id.id)
+            if line.scrap_id:
+                scrap_ids.append(line.scrap_id.id)
+
+        sql="""
+            delete from is_analyse_facturation
+            where invoice_date>=%s and invoice_date<=%s and bloquer='f'
+        """
+        cr.execute(sql,[date_debut, date_fin])
         filtre=[
-            ('invoice_date','>=',self.date_debut),
-            ('invoice_date','<=',self.date_fin),
+            ('invoice_date','>=',date_debut),
+            ('invoice_date','<=',date_fin),
             ('state','=','posted'),
         ]
         invoices = self.env['account.move'].search(filtre)
+        nb=len(invoices)
+        ct=0
         for invoice in invoices:
+            ct+=1
+            _logger.info("%s/%s : %s"%(ct,nb,invoice.name))
             for line in invoice.invoice_line_ids:
-                if line.price_subtotal>0.0:
-                    sens=1
-                    if invoice.move_type in ["out_refund","in_invoice"]:
-                        sens=-1
-                    vals={
-                        "invoice_id"    : invoice.id,
-                        "invoice_date"  : invoice.invoice_date,
-                        "partner_id"    : invoice.partner_id.id,
-                        "user_id"       : invoice.partner_id.user_id.id,
-                        "enseigne"      : invoice.partner_id.is_enseigne_id.name.name,
-                        "product_id"    : line.product_id.id,
-                        "product_uom_id": line.product_uom_id.id,
-                        "libelle"       : line.name,
-                        "quantity"      : line.quantity,
-                        "nb_colis"      : line.is_nb_colis or 0,
-                        "poids_net"     : line.is_poids_net or 0,
-                        "price_unit"    : line.price_unit,
-                        "price_subtotal": line.price_subtotal*sens,
-                        "move_type"     : _MOVE_TYPE[invoice.move_type],
-                    }
-                    self.env['is.analyse.facturation'].create(vals)
+                if line.id not in invoice_line_ids:
+                    if line.price_subtotal>0.0:
+                        sens=1
+                        if invoice.move_type in ["out_refund","in_invoice"]:
+                            sens=-1
+
+                        #** Recherche du dernier prix d'achat pour cet article ****
+                        prix_achat=montant_achat=marge_brute=0
+                        ligne_facture_fournisseur_id = date_facture_fournisseur = False
+                        if invoice.move_type=='out_invoice':
+                            sql="""
+                                SELECT  
+                                    aml.price_unit,
+                                    am.invoice_date,
+                                    aml.id
+                                FROM account_move_line aml inner join account_move am on aml.move_id=am.id
+                                WHERE 
+                                    am.invoice_date<=%s and
+                                    aml.product_id=%s and
+                                    am.move_type='in_invoice'
+                                ORDER BY am.invoice_date desc
+                                limit 1
+                            """
+                            cr.execute(sql,[invoice.invoice_date, line.product_id.id])
+                            for row in cr.fetchall():
+                                prix_achat                   = row[0]
+                                date_facture_fournisseur     = row[1]
+                                ligne_facture_fournisseur_id = row[2]
+                                montant_achat = prix_achat*line.quantity*sens
+                                marge_brute = line.price_subtotal*sens-montant_achat
+
+                        if invoice.move_type=='out_refund':
+                            marge_brute = line.price_subtotal*sens
+                        #**********************************************************
+
+                        vals={
+                            "invoice_id"     : invoice.id,
+                            "invoice_line_id": line.id,
+                            "invoice_date"  : invoice.invoice_date,
+                            "partner_id"    : invoice.partner_id.id,
+                            "user_id"       : invoice.partner_id.user_id.id,
+                            "enseigne"      : invoice.partner_id.is_enseigne_id.name.name,
+                            "product_id"    : line.product_id.id,
+                            "product_uom_id": line.product_uom_id.id,
+                            "libelle"       : line.name,
+                            "quantity"      : line.quantity,
+                            "nb_colis"      : line.is_nb_colis or 0,
+                            "poids_net"     : line.is_poids_net or 0,
+                            "price_unit"    : line.price_unit,
+                            "price_subtotal": line.price_subtotal*sens,
+                            "move_type"     : _MOVE_TYPE[invoice.move_type],
+                            "prix_achat"    : prix_achat,
+                            "montant_achat" : montant_achat,
+                            "marge_brute"   : marge_brute,
+                            "ligne_facture_fournisseur_id": ligne_facture_fournisseur_id,
+                            "date_facture_fournisseur"    : date_facture_fournisseur,
+                        }
+                        self.env['is.analyse.facturation'].create(vals)
 
         filtre=[
-            ('date_done','>=',self.date_debut),
-            ('date_done','<',self.date_fin+timedelta(1)),
+            ('date_done','>=',date_debut),
+            ('date_done','<',date_fin+timedelta(1)),
             ('state','=','done'),
         ]
         scraps = self.env['stock.scrap'].search(filtre)
+        nb=len(scraps)
+        ct=0
         for scrap in scraps:
-            price_unit = 0
-            partner_id=enseigne_id=user_id=False
-            for purchase in scrap.lot_id.purchase_order_ids:
-                partner_id  = purchase.partner_id.id
-                enseigne    = purchase.partner_id.is_enseigne_id.name.name
-                user_id     = purchase.partner_id.user_id.id
-                for line in purchase.order_line:
-                    if line.product_id == scrap.product_id:
-                        price_unit=line.price_unit
-            price_subtotal = -price_unit * scrap.scrap_qty
+            if scrap.id not in scrap_ids:
+                ct+=1
+                _logger.info("%s/%s : %s"%(ct,nb,scrap.name))
 
-            nb_colis = 0
-            poids_net = 0
-            if scrap.product_id.is_nb_pieces_par_colis>0:
-                nb_colis = scrap.scrap_qty / scrap.product_id.is_nb_pieces_par_colis
-                poids_net = nb_colis * scrap.product_id.is_poids_net_colis
+                price_unit = 0
+                partner_id=enseigne_id=user_id=False
+                for purchase in scrap.lot_id.purchase_order_ids:
+                    partner_id  = purchase.partner_id.id
+                    enseigne    = purchase.partner_id.is_enseigne_id.name.name
+                    user_id     = purchase.partner_id.user_id.id
+                    for line in purchase.order_line:
+                        if line.product_id == scrap.product_id:
+                            price_unit=line.price_unit
+                price_subtotal = -price_unit * scrap.scrap_qty
 
-            vals={
-                "scrap_id"      : scrap.id,
-                "invoice_date"  : scrap.date_done,
-                "partner_id"    : partner_id,
-                "user_id"       : user_id,
-                "enseigne"      : enseigne,
-                "product_id"    : scrap.product_id.id,
-                "product_uom_id": scrap.product_id.uom_id.id,
-                "libelle"       : scrap.name+" / "+(scrap.origin or ''),
-                "quantity"      : scrap.scrap_qty,
-                "nb_colis"      : nb_colis,
-                "poids_net"     : poids_net,
-                "price_unit"    : price_unit,
-                "price_subtotal": price_subtotal,
-                "move_type"     : "Rebut",
-            }
-            self.env['is.analyse.facturation'].create(vals)
+                nb_colis = 0
+                poids_net = 0
+                if scrap.product_id.is_nb_pieces_par_colis>0:
+                    nb_colis = scrap.scrap_qty / scrap.product_id.is_nb_pieces_par_colis
+                    poids_net = nb_colis * scrap.product_id.is_poids_net_colis
+
+                vals={
+                    "scrap_id"      : scrap.id,
+                    "invoice_date"  : scrap.date_done,
+                    "partner_id"    : partner_id,
+                    "user_id"       : user_id,
+                    "enseigne"      : enseigne,
+                    "product_id"    : scrap.product_id.id,
+                    "product_uom_id": scrap.product_id.uom_id.id,
+                    "libelle"       : scrap.name+" / "+(scrap.origin or ''),
+                    "quantity"      : scrap.scrap_qty,
+                    "nb_colis"      : nb_colis,
+                    "poids_net"     : poids_net,
+                    "price_unit"    : price_unit,
+                    "price_subtotal": price_subtotal,
+                    "marge_brute"   : price_subtotal,
+                    "move_type"     : "Rebut",
+                }
+                self.env['is.analyse.facturation'].create(vals)
 
         res= {
             'name': 'Analyse facturation',
@@ -112,12 +181,12 @@ class IsAnalyseFacturation(models.Model):
     _order = 'invoice_date desc,move_type'
 
 
-    invoice_date      = fields.Date('Date facture / Rebut', required=True)
+    invoice_date      = fields.Date("Date", help="Date facture, avoir ou rebut", required=True)
     invoice_id        = fields.Many2one('account.move', 'Facture')
+    invoice_line_id   = fields.Many2one('account.move.line', 'Ligne de Facture', index=True)
     scrap_id          = fields.Many2one('stock.scrap', 'Rebut')
     partner_id        = fields.Many2one('res.partner', 'Partenaire')
     user_id           = fields.Many2one('res.users', 'Vendeur')
-    #enseigne_id       = fields.Many2one('is.enseigne.commerciale', 'Enseigne')
     enseigne          = fields.Char('Enseigne')
     product_id        = fields.Many2one('product.product', 'Article')
     product_uom_id    = fields.Many2one('uom.uom', 'Unité')
@@ -127,6 +196,12 @@ class IsAnalyseFacturation(models.Model):
     nb_colis          = fields.Float(string='Nb colis' , digits=(14,1))
     price_unit        = fields.Float("Prix"    , digits='Product Price')
     price_subtotal    = fields.Float("Montant HT")
+
+    prix_achat        = fields.Float("Prix d'achat" , digits='Product Price')
+    montant_achat     = fields.Float("Montant achat")
+    marge_brute       = fields.Float("Marge brute")
+    ligne_facture_fournisseur_id = fields.Many2one('account.move.line', 'Ligne facture fournisseur')
+    date_facture_fournisseur     = fields.Date("Date facture fournisseur")
     move_type         = fields.Selection([
             ('Facture client'     , 'Facture client'),
             ('Avoir client'       , 'Avoir client'),
@@ -134,4 +209,12 @@ class IsAnalyseFacturation(models.Model):
             ('Avoir fournisseur'  , 'Avoir fournisseur'),
             ('Rebut'              , 'Rebut'),
         ], 'Type')
+    bloquer = fields.Boolean("Bloquer la mise à jour de cette fiche", help="A utiliser pour corriger une errreur dans Odoo", default=False, index=True)
 
+
+    @api.model
+    def analyse_facturation_ir_cron(self):
+        date_fin = date.today()
+        date_debut = date_fin - timedelta(30)
+        self.env['is.analyse.facturation.update'].update(date_debut,date_fin)
+        return True
