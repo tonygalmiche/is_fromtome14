174381a7af82b0320ae5a7f0eb3a82b72f26feb6
Tony Galmiche <tony.galmiche@infosaone.com>
2023-09-16 13:36:45 +0200

    modifs

--- models/mail_activity.py
+++ models/mail_activity.py
@@ -5,9 +5,49 @@ from odoo import api, exceptions, fields, models, _
 class MailActivity(models.Model):
     _inherit = 'mail.activity'
  
-    active = fields.Boolean("Actif", default=True)
+    active      = fields.Boolean("Actif", default=True)
+    partner_id  = fields.Many2one('res.partner','Partenaire', compute='_compute_partner_id', readonly=True, store=True)
+    is_customer = fields.Boolean("Est un Client"            , compute='_compute', readonly=True, store=True)
+    is_supplier = fields.Boolean("Est un Fournisseur"       , compute='_compute', readonly=True, store=True)
+
+
+    @api.depends('partner_id','partner_id.is_customer','partner_id.is_supplier')
+    def _compute(self):
+        for obj in self:
+            is_customer = obj.is_customer
+            is_supplier = obj.is_supplier
+            if obj.partner_id:
+                is_customer = obj.partner_id.is_customer
+                is_supplier = obj.partner_id.is_supplier
+            obj.is_customer = is_customer
+            obj.is_supplier = is_supplier
+
+
+    @api.depends('res_model','res_id')
+    def _compute_partner_id(self):
+        for obj in self:
+            partner_id= False
+            p=False
+            if obj.res_model=='res.partner':
+                partners=self.env['res.partner'].search([('id','=',obj.res_id)])
+                for partner in partners:
+                    p = partner
+            if obj.res_model=='stock.picking':
+                pickings=self.env['stock.picking'].search([('id','=',obj.res_id)])
+                for picking in pickings:
+                    p = picking.partner_id
+            if obj.res_model=='sale.order':
+                orders=self.env['sale.order'].search([('id','=',obj.res_id)])
+                for order in orders:
+                    p = order.partner_id
+            if p:
+                partner_id=p.id
+            obj.partner_id = partner_id
+            
 
 
     def unlink(self):
         self.active=False
         return True
+
+
