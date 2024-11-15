82bb7aecfba69b9ee7ce863c58214f73322b6415
Tony Galmiche <tony.galmiche@infosaone.com>
2024-11-15 11:54:25 +0100

    modifs

--- models/is_imprimer_etiquette_gs1.py
+++ models/is_imprimer_etiquette_gs1.py
@@ -5,7 +5,6 @@ import codecs
 import unicodedata
 import base64
 from datetime import datetime
-#import subprocess
 from subprocess import check_output, STDOUT, CalledProcessError
 
 class IsImprimanteEtiquette(models.Model):
@@ -16,6 +15,10 @@ class IsImprimanteEtiquette(models.Model):
     name      = fields.Char("Nom de l'imprimante", required=True)
     name_cups = fields.Char("Nom CUPS", required=True)
     default   = fields.Boolean("Imprimante par défaut", default=False)
+    dimension = fields.Selection([
+            ('101x76', '101.6 x 76.2'),
+            ('102x38', '102 x 38'),
+        ], 'Dimensions étiquette', default='101x76', required=True)
 
 
 class IsImprimerEtiquetteGS1(models.Model):
@@ -36,13 +39,6 @@ class IsImprimerEtiquetteGS1(models.Model):
     qt_imprime = fields.Integer("Quantité à Imprimer", required=True, default=1)
     imprimante_id = fields.Many2one('is.imprimante.etiquette', 'Imprimante étiquettes',default=lambda self: self.get_imprimante())
     alerte = fields.Text("Message")
-
-
-    # @api.constrains('poids')
-    # def constrains_poids(self):
-    #     for obj in self:
-    #         if obj.poids <= 0:
-    #             raise ValidationError("Le poids n'est pas renseigné")
 
 
     @api.model
@@ -77,74 +73,11 @@ class IsImprimerEtiquetteGS1(models.Model):
         return dt
 
 
-    @api.onchange('code_gs1','product_id')
-    def code_gs1_change(self):
-        self.alerte=False
-
-        # if self.code_gs1:
-        #     lines = self.code_gs1.strip().split('\n')
-        #     if len(lines)==1:
-        #         self.code_ean = lines[0].strip()
-        #     if len(lines)>1:
-        #         for line in lines:
-        #             line=line.strip()
-        #             if line:
-        #                 if line[:2] in ['00','01','02']:
-        #                     code_ean = line[2:]
-        #                     self.code_ean = code_ean
-        #                 if line[:2]=='10':
-        #                     self.lot = line[2:]
-    
-        #                 if line[:2]=='15':
-        #                     dluo = self.str2date(line[2:])
-        #                     self.dluo = dluo
-
-        #                 if line[:2]=='17':
-        #                     dlc = self.str2date(line[2:])
-        #                     self.dlc = dlc
-
-        #                 if line[:2]=='31':
-        #                     nb_decimales = float(line[3:4])
-        #                     if nb_decimales>0:
-        #                         poids = float(line[4:]) / (10**nb_decimales)
-        #                         self.poids = poids
-
-        #                 if line[:2]=='37':
-        #                     self.nb_pieces = int(line[2:])
-        # if self.code_ean:
-        #     products = self.env['product.product'].search([('barcode', '=', self.code_ean)])
-        #     for product in products:
-        #         self.product_id = product.id
-
-        if self.product_id and not self.lot:
-            self.lot=(self.product_id.default_code or '')+datetime.now().strftime("%j%y")
-
-        if self.product_id:
-            if self.product_id.barcode:
-                code_ean =  ((self.product_id.barcode or '')+"00000000000000")[:14]
-            else:
-                code_ean =  ((self.product_id.default_code or '')+"00000000000000")[:14]
-                alerte="Création d'un code EAN, car l'article sélectionné n'a pas de code actuellement"
-                self.alerte=alerte
-            self.code_ean = code_ean
 
 
-    # def generer_lot_action(self):
-    #     for obj in self:
-    #         obj.alerte=False
-    #         lot = (obj.product_id.default_code or '')+datetime.now().strftime("%j%y")
-    #         obj.lot=lot
-    #         alertes=[]
-    #         if obj.alerte:
-    #             alertes.append(obj.alerte)
-    #         alerte="Lot %s généré manuellement"%(lot)
-    #         alertes.append(obj.alerte)
-    #         obj.alerte="\n".join(alertes)
-
-
-    def imprimer_etiquette_action(self):
+    def get_ZPL(self):
         for obj in self:
-            obj.alerte=False
+            ZPL=''
             code_ean = self.code_ean or ''
             lot      = obj.lot or ''
            
@@ -157,13 +90,72 @@ class IsImprimerEtiquetteGS1(models.Model):
             gs1+=" (10)"+lot
             if obj.nb_pieces>1:
                 gs1+=" (37)"+("00"+str(obj.nb_pieces))[-2:]
-            ZPL="""
+
+            #** Petite étiquette **********************************************
+            if obj.imprimante_id.dimension=='102x38':
+
+                code_designation="%s - %s"%(obj.product_id.default_code,obj.product_id.name) #[0:50]
+                ligne1 = code_designation[0:50]
+                ligne2 = code_designation[50:99]
+                lot="LOT : %s"%lot
+                if obj.type_tracabilite=='dlc':
+                    dlc=''
+                    if obj.dlc:
+                        dlc=obj.dlc.strftime('%d/%m/%Y')
+                    dlc_ddm="DLC : %s"%dlc
+                else:
+                    ddm=''
+                    if obj.dluo:
+                          ddm=obj.dluo.strftime('%d/%m/%Y')
+                    dlc_ddm="DDM : %s"%ddm
+
+                pcb="PCB : %s"%obj.product_id.is_nb_pieces_par_colis
+                poids="POIDS NET : %.3fkg"%obj.product_id.is_poids_net_colis
+                ZPL="""
+^XA
+^CI28
+^BY3
+
+
+^CF0,44                       ^FX CF0 = Choix de la foncte (font 0) et taille de 44pt
+^FO50,50^FD%s^FS              ^FX Position X,Y et texte ligne1
+^FO50,100^FD%s^FS             ^FX Position X,Y et texte ligne2
+
+^FO50,170^FD%s^FS             ^FX Position X,Y et texte lot
+^FO50,220^FD%s^FS             ^FX Position X,Y et texte dlc/ddm
+
+^FO800,170^FD%s^FS             ^FX Position X,Y et texte pcb
+^FO800,220^FD%s^FS             ^FX Position X,Y et texte pouds
+
+^FO50,280                     ^FX Position X,Y du code barre
+^BCN,130,Y,N,,D               ^FX Code barre
+^FD%s^FS
+
+^XZ
+            """ % (
+                ligne1,
+                ligne2,
+                lot,
+                dlc_ddm,
+
+                pcb,
+                poids,
+
+
+                gs1
+            )
+
+
+
+            #** Grande étiquette **********************************************
+            if obj.imprimante_id.dimension=='101x76':
+                ZPL="""
 ^XA
 ^CI28
 ^BY3
 ^FO50,50^BCN,150,Y,N,,D
 ^FD%s^FS
-^CF0,40                                     ^FX CF0 = Choix de la foncte (font 0) et taille de 40pt
+^CF0,50                                     ^FX CF0 = Choix de la foncte (font 0) et taille de 40pt
 ^FO50,330^FDArticle : %s^FS                 ^FX Position et texte
 ^CF0,40                                     ^FX CF0 = Choix de la fonte (font 0) et taille de 30pt
 ^FO50,380^FD%s^FS                           ^FX Position et texte
@@ -185,6 +177,78 @@ class IsImprimerEtiquetteGS1(models.Model):
                 round(obj.poids,4),
                 obj.nb_pieces
             )
+
+            return ZPL
+
+
+
+    @api.onchange('code_gs1','product_id')
+    def code_gs1_change(self):
+        self.alerte=False
+
+        if self.product_id and not self.lot:
+            self.lot=(self.product_id.default_code or '')+datetime.now().strftime("%j%y")
+
+        if self.product_id:
+            if self.product_id.barcode:
+                code_ean =  ((self.product_id.barcode or '')+"00000000000000")[:14]
+            else:
+                code_ean =  ((self.product_id.default_code or '')+"00000000000000")[:14]
+                alerte="Création d'un code EAN, car l'article sélectionné n'a pas de code actuellement"
+                self.alerte=alerte
+            self.code_ean = code_ean
+
+    def imprimer_etiquette_action(self):
+        for obj in self:
+            obj.alerte=False
+
+
+#             code_ean = self.code_ean or ''
+#             lot      = obj.lot or ''
+           
+#             gs1 = "(01)"+code_ean
+#             if obj.dluo:
+#                 gs1+=" (15)"+obj.dluo.strftime("%y%m%d")
+#             if obj.dlc:
+#                 gs1+=" (17)"+obj.dlc.strftime("%y%m%d")
+#             gs1+=" (3103)"+("000000"+str(int(obj.poids*1000)))[-6:]
+#             gs1+=" (10)"+lot
+#             if obj.nb_pieces>1:
+#                 gs1+=" (37)"+("00"+str(obj.nb_pieces))[-2:]
+
+
+
+#             ZPL="""
+# ^XA
+# ^CI28
+# ^BY3
+# ^FO50,50^BCN,150,Y,N,,D
+# ^FD%s^FS
+# ^CF0,40                                     ^FX CF0 = Choix de la foncte (font 0) et taille de 40pt
+# ^FO50,330^FDArticle : %s^FS                 ^FX Position et texte
+# ^CF0,40                                     ^FX CF0 = Choix de la fonte (font 0) et taille de 30pt
+# ^FO50,380^FD%s^FS                           ^FX Position et texte
+# ^FO50,430^FDCode EAN (01) : %s^FS           ^FX Position et texte
+# ^FO50,480^FDLot (10) : %s^FS                ^FX Position et texte
+# ^FO50,580^FDDDM (15) : %s^FS                ^FX Position et texte
+# ^FO50,530^FDDLC (17) : %s^FS                ^FX Position et texte
+# ^FO50,630^FDPoids (3103) : %s^FS            ^FX Position et texte
+# ^FO50,680^FDNb pièces (37) : %s^FS
+# ^XZ
+#             """ % (
+#                 gs1,
+#                 obj.product_id.default_code,
+#                 obj.product_id.name,
+#                 code_ean,
+#                 lot,
+#                 obj.dluo or '',
+#                 obj.dlc or '',
+#                 round(obj.poids,4),
+#                 obj.nb_pieces
+#             )
+
+
+            ZPL = obj.get_ZPL()
             imprimante = obj.imprimante_id.name_cups or 'GX430T'
             name='etiquette-gs1-%s-zpl'%(imprimante)
             dest = '/tmp/'+name
@@ -193,7 +257,6 @@ class IsImprimerEtiquetteGS1(models.Model):
             f.close()
             cmd = "lpr -P %s %s "%(imprimante,dest)
             for x in range(0, obj.qt_imprime):
-                #subprocess.check_call(cmd, shell=True)
                 try:
                     output = check_output(cmd, shell=True, stderr=STDOUT)
                 except CalledProcessError as exc:
@@ -203,9 +266,6 @@ class IsImprimerEtiquetteGS1(models.Model):
                         alertes.append(obj.alerte)
                     alertes.append(msg)
                     obj.alerte="\n".join(alertes)
-                    #raise UserError()
-                #else:
-                #    assert 0
 
 
     def dupliquer_autre_poids_action(self):
@@ -220,10 +280,7 @@ class IsImprimerEtiquetteGS1(models.Model):
                 'name': "Dupliquer",
                 'view_mode': 'form',
                 'res_model': 'is.imprimer.etiquette.gs1',
-                #'view_id': self.env.ref('account.view_account_bnk_stmt_cashbox_footer').id,
                 'type': 'ir.actions.act_window',
-                #'res_id': cashbox_id,
                 'context': context,
-                #'target': 'new'
             }
             return action
